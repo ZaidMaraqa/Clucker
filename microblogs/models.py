0f4145348a27777d407b4b7d4aa15cdd79fc7fd4
Zaid Maraqa <zaid.maraqa@kcl.ac.uk>
2021-12-03 17:33:04 +0000

    Implement follow related methods of the user model

--- microblogs/models.py
+++ microblogs/models.py
@@ -16,6 +16,9 @@ class User(AbstractUser):
     last_name = models.CharField(max_length = 50, blank = False)
     email = models.EmailField(unique = True, blank = False)
     bio = models.CharField(max_length=520, blank = True)
+    followers = models.ManyToManyField(
+        'self', symmetrical=False, related_name='followees'
+    )
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
@@ -24,9 +27,30 @@ class User(AbstractUser):
         gravatar_object = Gravatar(self.email)
         gravatar_url = gravatar_object.get_image(size=size, default='mp')
         return gravatar_url
-        
+
     def mini_gravatar(self):
         return self.gravatar(size=60)
+
+    def toggle_follow(self, followee):
+        if self.is_following(followee):
+            self._unfollow(followee)
+        else:
+            self._follow(followee)
+
+    def _follow(self, user):
+        user.followers.add(self)
+
+    def _unfollow(self, user):
+        user.followers.remove(self)
+
+    def is_following(self, user):
+        return user in self.followees.all()
+
+    def follower_count(self):
+        return self.followers.count()
+
+    def followee_count(self):
+        return self.followees.count()
 
 
 
